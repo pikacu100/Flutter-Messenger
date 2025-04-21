3d4e46bfe5b94163481169caba5cb6b35484f859
pikacu100 <21sedlakmi@seznam.cz>
2025-04-21 17:50:39 +0200

    style

--- lib/screens/user/account.dart
+++ lib/screens/user/account.dart
@@ -47,94 +47,89 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    final gradientColors = isDarkMode
-        ? [
-            const Color(0xFF1E1E3F),
-            const Color(0xFF12122E),
-            const Color(0xFF0A0A1A),
-          ]
-        : [
-            const Color(0xFFF8F9FF),
-            const Color(0xFFE6F0FF),
-            const Color(0xFFD0E2FF),
-          ];
 
     return Scaffold(
+      backgroundColor: isDarkMode ? const Color(0xFF121212) : Colors.white,
       appBar: AppBar(
         centerTitle: true,
         titleTextStyle: FontStyles().appBarStyle(isDarkMode),
         title: const Text('Account'),
         surfaceTintColor: isDarkMode ? const Color(0xFF121212) : Colors.white,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: gradientColors,
-          stops: const [0.0, 0.5, 1.0],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        )),
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          children: [
-            SizedBox(
-              height: 100,
-              width: 100,
-              child: CircleAvatar(
-                backgroundColor:
-                    isDarkMode ? Colors.white : Colors.grey.shade900,
-                child: Text(
-                  nickname.isNotEmpty ? nickname[0].toUpperCase() : "A",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: isDarkMode ? Colors.black : Colors.white,
-                  ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        children: [
+          SizedBox(
+            height: 100,
+            width: 100,
+            child: CircleAvatar(
+              backgroundColor:
+                  isDarkMode ? Colors.white : Colors.grey.shade900,
+              child: Text(
+                nickname.isNotEmpty ? nickname[0].toUpperCase() : "A",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: isDarkMode ? Colors.black : Colors.white,
                 ),
               ),
             ),
-            const SizedBox(height: 20),
-            buildNicknameField(isDarkMode: isDarkMode),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          buildNicknameField(isDarkMode: isDarkMode),
+        ],
       ),
     );
   }
 
   Widget buildNicknameField({required bool isDarkMode}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Nickname",
-          style: TextStyle(
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-              color: isDarkMode ? Colors.grey.shade600 : Colors.grey.shade800),
-        ),
-        const SizedBox(height: 5),
-        TextFormField(
-          controller: _nicknameController,
-          maxLength: 30,
-          decoration: InputDecoration(
-            counterText: "",
-            border: const OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-              borderSide: BorderSide.none,
-            ),
-            filled: true,
-            fillColor: isDarkMode ? Colors.grey.shade900 : Colors.white,
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      decoration: BoxDecoration(
+        color: isDarkMode ? Colors.grey.shade900 : Colors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Nickname",
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: isDarkMode ? Colors.grey.shade600 : Colors.grey.shade800),
           ),
-          onChanged: (value) {
-            setState(() {
-              nickname = value;
-            });
-            if (_debounce?.isActive ?? false) _debounce?.cancel();
-            _debounce = Timer(const Duration(milliseconds: 2000), () {
-              _changeUserInfo();
-            });
-          },
-        ),
-      ],
+          const SizedBox(height: 5),
+          TextFormField(
+            controller: _nicknameController,
+            maxLength: 30,
+            style: TextStyle(
+              fontSize: 18,
+              color: isDarkMode ? Colors.grey.shade100 : Colors.black,
+              fontWeight: FontWeight.w500,
+            ),
+            decoration: InputDecoration(
+              counterText: "",
+              contentPadding: EdgeInsets.zero,
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: isDarkMode ? Colors.grey.shade900 : Colors.white,
+            ),
+            onChanged: (value) {
+              setState(() {
+                nickname = value;
+              });
+              if (_debounce?.isActive ?? false) _debounce?.cancel();
+              _debounce = Timer(const Duration(milliseconds: 2000), () {
+                _changeUserInfo();
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
